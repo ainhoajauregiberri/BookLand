7ed1684133c6aba54fe853e6f6000995191afb8e
Lorea13 <lorea.intxausti@opendeusto.es>
2020-11-02 13:06:03 +0100

    Sqlite

---
+++ src/sqlite/GestionBD.java
@@ -0,0 +1,183 @@
+package sqlite;
+
+
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+
+
+public class GestionBD {
+	
+	private String nombreFichero;
+	private String url="jdbc:sqlite:" + nombreFichero;
+	private Connection conn;
+	private SimpleDateFormat formatoFec;
+	
+	public GestionBD(String nombreFichero) {
+		this.nombreFichero = nombreFichero;
+		this.url = "jdbc:sqlite:" + nombreFichero;
+		this.conn = null;
+	}
+
+	public String getNombreFichero() {
+		return nombreFichero;
+	}
+
+	public void setNombreFichero(String nombreFichero) {
+		this.nombreFichero = nombreFichero;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
+
+	public SimpleDateFormat getFormatoFec() {
+		return formatoFec;
+	}
+
+	public void setFormatoFec(SimpleDateFormat formatoFec) {
+		this.formatoFec = formatoFec;
+	}
+
+	public void establecerConexion(){
+		
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void createDB() {
+		
+		establecerConexion();
+		
+		if (conn != null)
+        {
+			
+            DatabaseMetaData meta = null;
+			try {
+				meta = conn.getMetaData();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            try {
+				System.out.println("El nombre driver es: " + meta.getDriverName());
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            System.out.println("Se ha creado una nueva base de datos.");
+	}
+	}
+	
+	 public void crearModificarBorrarTabla(String sql){
+		 
+		 establecerConexion();
+		 Statement stmt = null;
+		 try {
+			stmt = conn.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		 
+		 try {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		 
+	 }
+	 
+	 public void insertarDatosPersona(int codPers, String nombre, String usuario, String contrasenya, String StringfecNac, String sexo){
+		 establecerConexion();
+		 PreparedStatement pstmt = null;
+		 
+		 String sql = "INSERT INTO Persona(codPers, nombre, usuario, contrasenya, fecNac, sexo) VALUES(?,?,?,?,?,?)";
+		 
+		 try {
+			pstmt = conn.prepareStatement(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 try {
+			pstmt.setInt(1, codPers);
+			pstmt.setString(2, nombre);
+			pstmt.setString(3, usuario);
+			pstmt.setString(4, contrasenya);
+			
+			Date fecNac = (java.sql.Date) string2Date("yyyy-MM-dd", StringfecNac);
+			
+			
+			pstmt.setDate(5, fecNac);
+			pstmt.setString(6, sexo);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		 
+		 try {
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
+	 
+	 public java.util.Date string2Date (String formato, String fecha){
+		
+		 this.formatoFec = new SimpleDateFormat(formato);
+
+		 try {
+			return this.formatoFec.parse(fecha);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+		    
+	 }
+	 
+	 //PONER TODOS!! ANIMO GIRLS. 
+	//ACUERDENSE DE QUE TODAS LAS FECHAS DEBEN LLAMAR A LA FUNCION STRING2DATE
+//SEGUN LA CLASE LA FECHA TENDRÁ UN FORMATO DIFERENTE
+
+
+public static void main (String [ ] args) {
+	 
+    GestionBD bd1=new GestionBD("usuarios.db");
+    bd1.establecerConexion();
+    bd1.createDB();
+    bd1.insertarDatosPersona(1, "Ainhoa", "ainhoa10", "kaixokaixo", "2000-07-10", "chica");
+
+	 }
+
+}
+	 
+	 
+
