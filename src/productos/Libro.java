74ba8c8cefc46e9f82090c96e81b7a5b769d203f
Lorea13 <lorea.intxausti@opendeusto.es>
2020-11-02 13:10:38 +0100

    Producto y Libro

---
+++ src/productos/Libro.java
@@ -0,0 +1,29 @@
+package productos;
+
+import productos.libros.Autor;
+import productos.libros.Genero;
+import servicios.ProductoUsuario;
+
+public class Libro extends Producto {
+	
+	private int edadRecLibro;
+
+	public int getEdadRecLibro() {
+		return edadRecLibro;
+	}
+
+	public void setEdadRecLibro(int edadRecLibro) {
+		this.edadRecLibro = edadRecLibro;
+	}
+
+	public Libro(boolean disponible, String tiutlo, Autor autor, Genero genero, ProductoUsuario usuario,
+			int edadRecLibro) {
+		super(disponible, tiutlo, autor, genero, usuario);
+		this.edadRecLibro = edadRecLibro;
+	}
+
+	
+	
+	
+	
+}
---
+++ src/productos/Producto.java
@@ -0,0 +1,60 @@
+package productos;
+import servicios.ProductoUsuario;
+import productos.libros.Autor;
+import productos.libros.Genero;
+
+public abstract class Producto {
+	private boolean disponible;
+	private String tiutlo;
+	private Autor autor;
+	private Genero genero;
+	private ProductoUsuario usuario;
+	
+	
+	
+	public Producto(boolean disponible, String tiutlo, Autor autor, Genero genero, ProductoUsuario usuario) {
+		super();
+		this.disponible = disponible;
+		this.tiutlo = tiutlo;
+		this.autor = autor;
+		this.genero = genero;
+		this.usuario = usuario;
+	}
+
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
+		this.disponible = disponible;
+	}
+	public String getTiutlo() {
+		return tiutlo;
+	}
+	public void setTiutlo(String tiutlo) {
+		this.tiutlo = tiutlo;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	public Genero getGenero() {
+		return genero;
+	}
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	public ProductoUsuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(ProductoUsuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	
+
+}
