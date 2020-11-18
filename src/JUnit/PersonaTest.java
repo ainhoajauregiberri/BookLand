cbea3fdfd2efff7d62715c5d5eb460cfaa44fd9c
ainhoajauregiberri <ainhoaillarramendi@opendeusto.es>
2020-11-18 09:11:49 +0100

    junit nuevo

---
+++ src/JUnit/PersonaTest.java
@@ -0,0 +1,30 @@
+package JUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import personas.Persona;
+
+public class PersonaTest {
+	
+	private Persona persona;
+
+	@Before
+	public void setUp() throws Exception {
+		persona = new Persona("lourdes","lourdes10","943375432","1964-09-09","chica");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		persona=null;
+	}
+
+	@Test
+	public void testComprobarUsuario() {
+		assertEquals(persona.getNombre(), "lourdes");
+	}
+
+}
